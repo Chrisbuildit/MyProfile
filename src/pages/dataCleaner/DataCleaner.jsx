e77310a9dcc93246a499887b7ce52a634601b734
Chrisbuildit <chris@DESKTOP-92P9M55>
2024-05-06 13:41:35 +0200

    update

--- src/pages/dataCleaner/DataCleaner.jsx
+++ src/pages/dataCleaner/DataCleaner.jsx
@@ -6,17 +6,22 @@ function DataCleaner () {
     return (
         <div>
             <div className='Grid-display'>
-                <body>
-                <h3>Data Cleaner - Paardekooper</h3>
-                    <ul>
-                        <li>Verschonen van product informatie.</li>
-                        <li>Aanvragen van ontbrekende product informatie bij leveranciers.</li>
-                        <li>Opstellen van dashboard met behulp van Pivot tables in Excel.</li>
-                        <li>Matchen van item codes mbv v-lookup en index&match formule.</li>
-                        <li>Collega’s en leidinggevende helpen met excel.</li>
-                        <li>Data bewerken met Power Query.</li>
-                    </ul>
-                </body>
+                <div className='test'>
+                    <section>
+                        <h3>Data Cleaner <span>- Paardekooper</span></h3>
+                        <ul>
+                            <li>Verschonen van product informatie.</li>
+                            <li>Aanvragen van ontbrekende product informatie bij leveranciers.</li>
+                            <li>Opstellen van dashboard met behulp van Pivot tables in Excel.</li>
+                            <li>Matchen van item codes mbv v-lookup en index&match formule.</li>
+                            <li>Collega’s en leidinggevende helpen met excel.</li>
+                            <li>Data bewerken met Power Query.</li>
+                        </ul>
+                    </section>
+                    <section className='section'>
+                        <p><b>Referentie: </b>jan@vanholten.com (projectleider) </p>
+                    </section>
+                </div>
                 <Link to="https://www.paardekooper.nl/">
                     <img className='image' src={image} alt=""/>
                 </Link>            
