9bda5c617c66301046b5ed6846ffd567e07ac00c
Chrisbuildit <chris@DESKTOP-92P9M55>
2025-03-06 22:45:12 +0100

    update

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
-
+import { gapi } from 'gapi-script';
+import { useEffect, useRef } from 'react';
 
 function CV () {
+
+//Werkt niet
+const sheetElement = useRef(null);
+
+useEffect(() => {
+  if (sheetElement.current) {
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/api.js';
+    document.head.appendChild(script);
+    script.onload = () => {
+      gapi.load('client:auth2', () => {
+        gapi.client.setApiKey('AIzaSyChgAlo-lfYkKCyDlCX7WYg89Wiu1m3fpY');
+        gapi.client.load('sheets', 'v4', () => {
+          const sheetId = '14WUiigHAgcYWXdTzWNJtjxkO_fYH2ssNOmYRZ5YlVdEywS77ViKliKjK';
+          const tabbladen = document.querySelectorAll(`.docs-sheet-${sheetId} .docs-sheet-tab`);
+          tabbladen.forEach(tab => {
+            tab.className = 'hidden';
+          });
+        });
+      });
+    };
+  }
+}, []);
+
     return (
-        <div className='iframe'>            
-            <iframe className="CV" title='CV' src="https://docs.google.com/document/d/1gMY2XgNAmYNv9fomwHy8h9lEFNDqzBwlZHd6INLN0E4/edit?usp=sharing"></iframe>
+        <div className='iframe' ref={sheetElement}>      
+            <iframe className="CV" title='Google Sheet' src="https://docs.google.com/document/d/1gMY2XgNAmYNv9fomwHy8h9lEFNDqzBwlZHd6INLN0E4/edit?usp=sharing"></iframe>
         </div>
     )
 }
