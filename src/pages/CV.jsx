f4794f01138209d53f960d758cb0353ca8d22945
Chrisbuildit <chris@DESKTOP-92P9M55>
2025-03-08 11:56:27 +0100

    update

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from 'react';
 
 function CV () {
 
-//Werkt niet
+// Werkt niet (Veroorzaakt geen error)
 const sheetElement = useRef(null);
 
 useEffect(() => {
@@ -14,7 +14,7 @@ useEffect(() => {
     document.head.appendChild(script);
     script.onload = () => {
       gapi.load('client:auth2', () => {
-        gapi.client.setApiKey('AIzaSyChgAlo-lfYkKCyDlCX7WYg89Wiu1m3fpY');
+        gapi.client.setApiKey(process.env.REACT_APP_API_KEY);
         gapi.client.load('sheets', 'v4', () => {
           const sheetId = '14WUiigHAgcYWXdTzWNJtjxkO_fYH2ssNOmYRZ5YlVdEywS77ViKliKjK';
           const tabbladen = document.querySelectorAll(`.docs-sheet-${sheetId} .docs-sheet-tab`);
@@ -26,9 +26,9 @@ useEffect(() => {
     };
   }
 }, []);
-
+ 
     return (
-        <div className='iframe' ref={sheetElement}>      
+        <div className='iframe' >      
             <iframe className="CV" title='Google Sheet' src="https://docs.google.com/document/d/1gMY2XgNAmYNv9fomwHy8h9lEFNDqzBwlZHd6INLN0E4/edit?usp=sharing"></iframe>
         </div>
     )
